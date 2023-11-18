53ef24e8aca4a7c51202405f3260e02ed7476c05
L-Ewart <81682726+L-Ewart@users.noreply.github.com>
2023-11-18 13:53:15 +0000

    Update newTest.c

--- newTest.c
+++ newTest.c
@@ -15,17 +15,11 @@ int readNumOfCoords();
 double **readCoords();
 void *writeTourToFile();
 #endif
+
 // Provided functions
 int readNumOfCoords(char *filename);
 double **readCoords(char *filename, int numOfCoords);
 void *writeTourToFile(int *tour, int tourLength, char *filename);
-
-
-// Custom reduction function
-/* MinCostData minCostFunc(MinCostData a, MinCostData b) {
-    return (a.cost < b.cost) ? a : b;
-} */
-
 
 // Function to calculate the Euclidean distance between two points
 double distance(double x1, double y1, double x2, double y2) {
@@ -33,14 +27,12 @@ double distance(double x1, double y1, double x2, double y2) {
 }
 
 // Function to generate a distance matrix from coordinates
-
-
-
 double **generateDistanceMatrix(double **coords, int numOfCoords) {
     double **matrix = (double **)malloc(numOfCoords * sizeof(double *));
     for (int i = 0; i < numOfCoords; i++) {
         matrix[i] = (double *)malloc(numOfCoords * sizeof(double));
     }
+
     #pragma omp parallel for
     for (int i = 0; i < numOfCoords; i++) {
         matrix[i][i] = 0; // Distance from a point to itself is 0
@@ -49,15 +41,16 @@ double **generateDistanceMatrix(double **coords, int numOfCoords) {
             matrix[j][i] = matrix[i][j]; // Use symmetry, avoid redundant calculation
         }
     }
+
     return matrix;
 }
 
-     void findNearestNeighbor(double **distanceMatrix, int numOfCoords, int currentNode, int *nearestIndex, double *nearestDistance) {
+// Function to find the nearest neighbor
+void findNearestNeighbor(double **distanceMatrix, int numOfCoords, int currentNode, int *nearestIndex, double *nearestDistance) {
     double localNearestDistance = DBL_MAX;
     int localNearestIndex = -1;
 
-    // Parallel for loop to find the minimum distance
-    #pragma omp parallel for reduction(min:localNearestDistance)
+    #pragma omp parallel for reduction(min : localNearestDistance)
     for (int i = 0; i < numOfCoords; i++) {
         if (i != currentNode) {
             double dist = distanceMatrix[currentNode][i];
@@ -77,75 +70,66 @@ double **generateDistanceMatrix(double **coords, int numOfCoords) {
 
     *nearestDistance = localNearestDistance;
     *nearestIndex = localNearestIndex;
-} 
+}
 
 int *cheapestInsertion(double **distanceMatrix, int numOfCoords) {
     int *tour = malloc((numOfCoords + 1) * sizeof(int));
     int *unvisited = malloc(numOfCoords * sizeof(int));
     int unvisitedCount = numOfCoords - 1;
-    
+
     // Initialize unvisited nodes
     for (int i = 1; i < numOfCoords; i++) {
         unvisited[i - 1] = i;
     }
+
     // Step 1: Start with vertex 0
     tour[0] = 0;
     int tourSize = 1;
+
     // Step 2: Find the nearest neighbor to 0
     double nearestDistance;
     int nearestIndex;
-    findNearestNeighbor(distanceMatrix, numOfCoords, 0, &nearestIndex, &nearestDistance);  
-        //int nearest = INT_MAX, nearestIndex = -1;
+    findNearestNeighbor(distanceMatrix, numOfCoords, 0, &nearestIndex, &nearestDistance);
+
     tour[1] = nearestIndex;
     tour[2] = 0; // Close the loop
     tourSize = 3;
-    
 
-    int numThreads = omp_get_max_threads();
-    double *localMinCosts = malloc(numThreads * sizeof(double));
-    int *localMinCostIndices = malloc(numThreads * sizeof(int));
-    int *localInsertPositions = malloc(numThreads * sizeof(int));
     while (tourSize < numOfCoords + 1) {
-        double globalMinCost = DBL_MAX; 
+        double globalMinCost = DBL_MAX;
         int globalMinCostIndex = -1, globalInsertPosition = -1;
+
         #pragma omp parallel
         {
-
             int threadID = omp_get_thread_num();
-            localMinCosts[threadID] = DBL_MAX;
-            localMinCostIndices[threadID] = -1;
-            localInsertPositions[threadID] = -1;
+            double localMinCost = DBL_MAX;
+            int localMinCostIndex = -1, localInsertPosition = -1;
 
             #pragma omp for nowait
             for (int idx = 0; idx < unvisitedCount; idx++) {
-                        int i = unvisited[idx];
-                    for (int j = 0; j < tourSize - 1; j++) {
-                        int cost = distanceMatrix[tour[j]][i] + distanceMatrix[i][tour[j + 1]] - distanceMatrix[tour[j]][tour[j + 1]];
-                        if (cost < localMinCosts[threadID]) {
-                            localMinCosts[threadID] = cost;
-                            localMinCostIndices[threadID] = i;
-                            localInsertPositions[threadID] = j + 1;
-                        }
+                int i = unvisited[idx];
+                for (int j = 0; j < tourSize - 1; j++) {
+                    double cost = distanceMatrix[tour[j]][i] + distanceMatrix[i][tour[j + 1]] - distanceMatrix[tour[j]][tour[j + 1]];
+                    if (cost < localMinCost) {
+                        localMinCost = cost;
+                        localMinCostIndex = i;
+                        localInsertPosition = j + 1;
                     }
-                
+                }
             }
+
             #pragma omp critical
             {
-                if (localMinCosts[threadID] < globalMinCost) {
-                globalMinCost = localMinCosts[threadID];
-                globalMinCostIndex = localMinCostIndices[threadID];
-                globalInsertPosition = localInsertPositions[threadID];
+                if (localMinCost < globalMinCost) {
+                    globalMinCost = localMinCost;
+                    globalMinCostIndex = localMinCostIndex;
+                    globalInsertPosition = localInsertPosition;
                 }
             }
         }
-        
-           
-        
-        
-    
 
-                // Update the tour outside the parallel region
-            if (globalMinCostIndex != -1) {
+        // Update the tour outside the parallel region
+        if (globalMinCostIndex != -1) {
             // Shift elements to the right to make space for the new node
             for (int i = tourSize; i > globalInsertPosition; i--) {
                 tour[i] = tour[i - 1];
@@ -158,25 +142,16 @@ int *cheapestInsertion(double **distanceMatrix, int numOfCoords) {
             for (int i = 0; i < unvisitedCount; i++) {
                 if (unvisited[i] == globalMinCostIndex) {
                     unvisited[i] = unvisited[unvisitedCount - 1]; // Swap with the last element
-                    unvisitedCount--; // Reduce the count of unvisited nodes
+                    unvisitedCount--;                               // Reduce the count of unvisited nodes
                     break;
                 }
             }
         }
-            
-        
     }
-    
+
     free(unvisited);
-    free(localMinCosts);
-    free(localMinCostIndices);
-    free(localInsertPositions);
     return tour;
 }
-
- 
-
-
 
 int main(int argc, char *argv[]) {
     // Ensure correct usage
@@ -191,7 +166,7 @@ int main(int argc, char *argv[]) {
     // Read coordinates
     int numOfCoords = readNumOfCoords(inputFile);
     double **coords = readCoords(inputFile, numOfCoords);
-    clock_t start_time = clock();
+
     // Generate distance matrix
     double **distanceMatrix = generateDistanceMatrix(coords, numOfCoords);
 
@@ -200,30 +175,11 @@ int main(int argc, char *argv[]) {
 
     // Write the tour to the output file
     writeTourToFile(tour, numOfCoords + 1, outputFile);
-    clock_t end_time = clock();
-    double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
 
-   FILE *file = fopen("timelapsed", "w"); // Open the file once outside the loop
-    if (file == NULL) {
-        perror("Error opening file");
-    return EXIT_FAILURE;
-    }
-
-// Your processing logic here...
-
-fprintf(file, "Elapsed time: %f seconds\n", elapsed_time); // Example of writing to the file
-
-fclose(file); // Close the file
     // Free memory
     for (int i = 0; i < numOfCoords; i++) {
-        //free(coords[i]);
         free(distanceMatrix[i]);
-
-    
     }
-    
-    
-    //free(coords);
     free(distanceMatrix);
     free(tour);
 
